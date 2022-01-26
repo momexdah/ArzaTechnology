194e127c447557fc8427de2ae6d7335d4f10b467
Diomedes Arribasplata <darribasplatah17_1@unc.edu.pe>
2022-01-25 23:04:49 -0500

    Mejoras en imagenes y compras

--- forms/contact.php
+++ forms/contact.php
@@ -9,7 +9,8 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'ventas@arzatechnology.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  $php_email_form = '../assets/vendor/php-email-form/php-email-form.php';
+  if( file_exists($php_email_form)) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
